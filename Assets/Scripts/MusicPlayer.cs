762c6e91e092848843316ad9201cdda5f050807d
moskitgp <moskitgp@gmail.com>
2018-01-24 22:17:52 +0100

    music player and some clearings
    I added music player (that's not functioning right now ;_;) and made some clearing on project panel

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+
+	public AudioClip[] clips;
+	private AudioSource audioSource;
+
+	void Start () {
+
+		audioSource = GetComponent<AudioSource> ();
+		audioSource.loop = false;
+		//audioSource.volume = 0.5f;
+		
+	}
+
+	private AudioClip GetRandomClip(){
+		return clips[Random.Range(0, 5)];
+	}
+
+	void Update () {
+		if (audioSource.isPlaying) {
+			audioSource.clip = GetRandomClip ();
+			audioSource.Play ();
+		}
+	}
+}
