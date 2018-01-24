beaf38b6221ca341d48fe52810b08ca545f49654
Michał Rogowski <moskitgp@gmail.com>
2018-01-24 18:42:20 +0100

    Small change of project files
    deleting useless files

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -8,7 +8,7 @@ public class MainMenu : MonoBehaviour {
 
 	public void PlayGame()
 	{
-		SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex + 1);
+		SceneManager.LoadScene ("ClickerScene");
 	}
 
 
