53936042dff68f72462bf994bf53d36516525818
moskitgp <moskitgp@gmail.com>
2018-03-14 17:41:35 +0100

    Pausemenu initial coding + clicking sfx

---
+++ Assets/Scripts/SaveManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveManager : MonoBehaviour {
+
+	public GameObject upgrade1, upgrade2, upgrade3, upgrade4, upgrade5;
+
+	void Start()
+	{
+		//int upgradeLevel1 = upgrade1.GetComponent<UpgradeManager> ().upgradeLevel;
+	}
+
+	/*public void SaveGame (){
+		(PlayerPrefs.SetInt("
+		
+	}*/
+
+
+}
